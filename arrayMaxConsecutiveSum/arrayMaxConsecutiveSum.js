6423f317af5c95e58699aa0a68c12ec19a6c40da
Ganek <andrzej_00@interia.eu>
2019-03-27 21:44:49 +0100

    avoidObstacles finished

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
@@ -1,17 +1,18 @@
-function arrayMaxConsecutiveSum(inputArray, k,) {
+function arrayMaxConsecutiveSum(inputArray, k) {
     let sum = 0;
-    let max= 0;
+    let max = 0;
 
     for (let i = 0; i < k; i++) {
         sum += inputArray[i];
     }
+    console.log(sum);
     max = sum;
-
     for (let i = k; i < inputArray.length; i++) {
+
         sum -= inputArray[i - k];
         sum += inputArray[i];
-        if (sum>max){
-            max=sum
+        if (sum > max) {
+            max = sum
         }
     }
     return max;
